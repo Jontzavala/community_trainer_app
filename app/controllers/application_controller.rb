4bffa68491065913b0c3ef33e2dfe801319e644b
Jonathan Zavala <jontzavala@gmail.com>
2021-02-27 17:11:59 -0600

    Set up migrations and Models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  #use Rack::Flash
 
   configure do
+    #enable :sessions
+    #set :session_secret, 'thebestsecretever'
     set :public_folder, 'public'
     set :views, 'app/views'
   end
