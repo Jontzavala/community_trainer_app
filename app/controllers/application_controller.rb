576b4bc237311ec439afcb68f07d426ad2082ee9
Jonathan Zavala <jontzavala@gmail.com>
2021-02-27 17:37:55 -0600

    Set up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,41 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-  #use Rack::Flash
 
   configure do
-    #enable :sessions
-    #set :session_secret, 'thebestsecretever'
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    register Sinatra::Flash
+    set :session_secret, 'thebestsecreteverofalltime'
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect to '/workouts'
+    else
+      erb :index
+    end
+  end
+
+  helpers do
+
+    def logged_in?
+      !!sessions[:user_id]
+    end
+    
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+
+    def redirect_to_if_logged_in
+      redirect '/workouts' if logged_in?
+    end
+
+    def redirect_to_if_not_logged_in
+      redirect '/login' unless logged_in?
+    end
+  
   end
 
 end
