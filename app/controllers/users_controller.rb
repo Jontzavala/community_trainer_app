55888e1b0cb02db7859376672084b5c1e8db3a60
Jonathan Zavala <jontzavala@gmail.com>
2021-03-02 15:20:11 -0600

    Finished user_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UserController < ApplicationController
+    get '/signup' do
+        if logged_in?
+            redirect to '/workouts'
+        else
+            erb :'users/signup'
+        end
+    end
+
+    post '/signup' do
+        user = User.new(params)
+        if user.save
+            flash[:notice] = "You have succesfully signed up! Lets get in shape!"
+            sesssion[:user_id] = user.id
+            redirect to '/workouts'
+        else
+            flash.now[:error] = "Something went wrong, pleas try again"
+            erb :'users/signup'
+        end
+    end
+
+    get '/login' do
+        if logged_in?
+            redirect to '/workouts'
+        else
+            erb :'users/login'
+        end
+    end
+
+    post '/login' do
+        @current_user = User.find_by(:username => params[:username], :email => params[:email])
+        if @current_user && @current_user.authenticate(params[:password])
+            sesssion[:user_id] = @current_user.id
+            flash[:notice] = "You're logged in! Lets start working out!"
+            redirect to '/workouts'
+        else
+            if @current_user
+                flash.now[:error] = "Something went wrong, please try again"
+                erb :'users/login'
+            else
+                flash.now[:error] = "Something went wrong, please try again"
+                erb :'users/login'
+            end
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.clear
+            redirect to '/'
+        else
+            redirect to '/'
+        end
+    end
+
+end
