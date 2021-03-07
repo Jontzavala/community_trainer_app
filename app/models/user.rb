cee63899ce11244799648450cee9775bae12af3e
Jonathan Zavala <jontzavala@gmail.com>
2021-03-07 15:46:50 -0600

    fixed a password bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates :password, presence:true, uniqueness: true
     validates :username, presence:true, uniqueness: true 
     validates :email, presence:true, uniqueness: true
     has_many :workouts 
